9acd0529307fb4ebbb398639daa920adcf37a47f
alobam01 <alobamlucas@gmail.com>
2026-03-17 22:55:20 +0100

    first

--- scripts/test-env.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-console.log('🔍 Testing environment variables...');
-console.log('NEXT_PUBLIC_SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL ? 'Set' : 'Not set');
-console.log('SUPABASE_SERVICE_ROLE_KEY:', process.env.SUPABASE_SERVICE_ROLE_KEY ? 'Set' : 'Not set');
-console.log('ADMIN_EMAIL:', process.env.ADMIN_EMAIL ? 'Set' : 'Not set');
